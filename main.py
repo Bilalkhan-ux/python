c40336cefb4d0b28be27be4c41fa9361e1556ff7
Mr President <bilalkhanwn2@gmail.com>
2026-08-06 10:59:46 +0500

    type conv/casting added

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ print ("Hello")
 age = 21
 print("My name is Bilal and my age is " , age)
 
-name = input("Enter name: ")
 
-print("Hello ", name)
+age = input("Enter age: ")  
+print("Age: ",age, type(age))
+#anything type of data we take input will be stored as "str"
+
+age = int(age) # type casting
+print(age, type(age))
+
+num = 255
+print(num+2.5) #type conversion
+
+#sum program
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+sum = a+b
+print("Total: ", sum)

a11d544b712cb9ad7069208aaff619eaec35afea
Mr President <bilalkhanwn2@gmail.com>
2026-08-07 12:13:39 +0500

    tuple added

---
+++ tuple.py
@@ -0,0 +1,12 @@
+#same as list but immutable / can't be changed
+
+marks = (67,65,76,54,86,99,56,99, 74,99) #it uses (), list use []
+
+print(marks)
+
+#count occurence
+
+print(marks.count(99))
+
+# to print index 
+print(marks.index(86))
